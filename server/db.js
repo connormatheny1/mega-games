b049379b65b0c54bf91e6c17146f7915c72a1127
connormatheny <mathenc@miamioh.edu>
2020-04-18 12:36:55 -0500

    remove sensitive info, plz dont look in other commits thanks :)

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const { Pool, Client } = require('pg')
 
-const pool = new Pool({ connectionString: 'postgres://connormatheny@localhost:5432/games', ssl: false})
+const pool = new Pool({ connectionString: process.env.DB_DEV, ssl: false})
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
