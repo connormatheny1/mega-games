4037c48f15e4d6b86eab98308c502a57fd5bac41
connormatheny <mathenc@miamioh.edu>
2020-05-08 18:58:25 -0500

    add static build route

--- index.js
+++ index.js
@@ -86,4 +86,15 @@ app.use(bodyParser.json())
 app.use('/', indexRouter)
 app.use('/users', userRouter)
 
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
