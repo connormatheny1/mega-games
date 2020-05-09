17047894df047ef28fc50dc6eab209e8ae5c9ead
connormatheny <mathenc@miamioh.edu>
2020-05-08 21:21:24 -0500

    change pg connect string, need to understand environment changing and vars dynamically

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool, Client } = require('pg')
 
-const pool = new Pool({ connectionString: process.env.DB_DEV, ssl: false})
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: false})
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
