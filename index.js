a7fe59b7a3113aad1713c30046d0cf4fbb866d3c
connormatheny <mathenc@miamioh.edu>
2020-05-08 19:45:50 -0500

    change route sending

--- index.js
+++ index.js
@@ -89,14 +89,14 @@ app.use('/', indexRouter)
 app.use('/users', userRouter)
 
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
   
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  }
+//     // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+//   }
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path')
 const bcrypt = require('bcrypt');
 const db = require('../db');
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('index server is good')
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 module.exports = router
