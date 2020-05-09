5835523b78b3cfdcb6a1053006a707f40f8d3161
connormatheny <mathenc@miamioh.edu>
2020-05-08 20:58:58 -0500

    hello maybe work

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.set('port', PORT)
 
 app.use(express.urlencoded({extended: true}))
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 
 io.of(process.env.NAMESPACE).on('connection', (socket) => {
